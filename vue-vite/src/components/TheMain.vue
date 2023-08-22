11cd35684e72cf045e808c9b6babadb19866f614
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-08-22 18:47:34 +0200

    fix padding main

--- vue-vite/src/components/TheMain.vue
+++ vue-vite/src/components/TheMain.vue
@@ -6,28 +6,28 @@
 <template>
   <main>
     <div class="container-fluid bg-primary">
-      <div class="container d-flex justify-content-between align-items-center py-5 text-white">
-        <div class="d-flex align-items-center gap-3">
+      <div id="custom-main-padding" class="container d-flex justify-content-between align-items-center py-5 text-white">
+        <div class="d-flex align-items-center gap-2">
           <img src="../../vue-dc-comics-1/img/buy-comics-digital-comics.png" alt="Digital Comic">
           <h3 class="text-uppercase fs-6">digital comics</h3>
         </div>
 
-        <div class="d-flex align-items-center gap-3">
+        <div class="d-flex align-items-center gap-2">
           <img src="../../vue-dc-comics-1/img/buy-comics-merchandise.png" alt="Digital Comic">
           <h3 class="text-uppercase fs-6">digital merchandise</h3>
         </div>
 
-        <div class="d-flex align-items-center gap-3">
+        <div class="d-flex align-items-center gap-2">
           <img src="../../vue-dc-comics-1/img/buy-comics-subscriptions.png" alt="Digital Comic">
           <h3 class="text-uppercase fs-6">subscription</h3>
         </div>
 
-        <div class="d-flex align-items-center gap-3">
+        <div class="d-flex align-items-center gap-2">
           <img src="../../vue-dc-comics-1/img/buy-comics-shop-locator.png" alt="Digital Comic">
           <h3 class="text-uppercase fs-6">comic shop locator</h3>
         </div>
 
-        <div class="d-flex align-items-center gap-3">
+        <div class="d-flex align-items-center gap-2">
           <img src="../../vue-dc-comics-1/img/buy-dc-power-visa.svg" alt="Digital Comic">
           <h3 class="text-uppercase fs-6">dc power visa</h3>
         </div>
@@ -39,5 +39,9 @@
 <style lang="scss" scoped>
   img {
     height: 50px;
+  }
+
+  #custom-main-padding {
+    padding: 0 5rem;
   }
 </style>
